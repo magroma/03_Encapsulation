51e5bbbcbc846eac22545b5ded688b73fc689f9a
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-06-01 18:58:47 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,15 @@
+
+public class App {
+
+	public static void main(String[] args) {
+	
+		ausgabe("hu");
+	
+	}
+	
+	
+	public static void ausgabe(String outStr){
+		System.out.println(outStr);
+	}
+
+}
